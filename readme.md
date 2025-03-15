97a7f4cef68476187ad156679b9a3dc8b8207c27
vablings <47798035+vablings@users.noreply.github.com>
2025-03-14 21:19:44 -0500

    added optional sizing of struct with padding

--- readme.md
+++ readme.md
@@ -38,72 +38,13 @@ offset_debug!(
     }
 );
 ```
-#### VMCB State save area as seen in SVM based hypervisors.
+
+#### UWorld using optional sizing argument
 ```rust
 offset_debug!(
-    pub struct SegmentRegister {
-        0x0 pub selector: u16,
-        0x2 pub attrib: u16,
-        0x4 pub limit: u32,
-        0x8 pub base: u64,
-    }
-);
-
-offset_debug!(
-    pub struct VmcbSave {
-        0x0   pub es: SegmentRegister,
-        0x10  pub cs: SegmentRegister,
-        0x20  pub ss: SegmentRegister,
-        0x30  pub ds: SegmentRegister,
-        0x40  pub fs: SegmentRegister,
-        0x50  pub gs: SegmentRegister,
-        0x60  pub gdtr: SegmentRegister,
-        0x70  pub ldtr: SegmentRegister,
-        0x80  pub idtr: SegmentRegister,
-        0x90  pub tr: SegmentRegister,
-        0xcb  pub cpl: u8,
-        0xd0  pub efer: u64,
-        0x148 pub cr4: u64,
-        0x150 pub cr3: u64,
-        0x158 pub cr0: u64,
-        0x160 pub dr7: u64,
-        0x168 pub dr6: u64,
-        0x170 pub rflags: u64,
-        0x178 pub rip: u64,
-        0x1d8 pub rsp: u64,
-        0x1e0 pub s_cet: u64,
-        0x1e8 pub ssp: u64,
-        0x1f0 pub isst_addr: u64,
-        0x1f8 pub rax: u64,
-        0x200 pub star: u64,
-        0x208 pub lstar: u64,
-        0x210 pub cstar: u64,
-        0x218 pub sfmask: u64,
-        0x220 pub kernel_gs_base: u64,
-        0x228 pub sysenter_cs: u64,
-        0x230 pub sysenter_esp: u64,
-        0x238 pub sysenter_eip: u64,
-        0x240 pub cr2: u64,
-        0x268 pub g_pat: u64,
-        0x270 pub dbgctl: u64,
-        0x278 pub br_from: u64,
-        0x280 pub br_to: u64,
-        0x288 pub last_excp_from: u64,
-        0x298 pub dbg_extn_cfg: u64,
-        0x2e0 pub spec_ctrl: u64,
-        // FIXME Better to keep this from printing at all by implementing Display
-        // 0x670 lbr_stack: [u8; 0x100],
-        0x770 pub lbr_select: u64,
-        0x778 pub ibs_fetch_ctl: u64,
-        0x780 pub ibs_fetch_linaddr: u64,
-        0x788 pub ibs_op_ctl: u64,
-        0x790 pub ibs_op_rip: u64,
-        0x798 pub ibs_op_data: u64,
-        0x7a0 pub ibs_op_data2: u64,
-        0x7a8 pub ibs_op_data3: u64,
-        0x7b0 pub ibs_dc_linaddr: u64,
-        0x7b8 pub bp_ibstgt_rip: u64,
-        0x7c0 pub ic_ibs_extd_ctl: u64,
+    pub struct UWorld[0x08E8] {
+        0x30 pub PersistentLevel: Pointer64<ULevel>,
     }
 );
 ```
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,75 @@
 pub extern crate paste;
 
 #[macro_export]
-/// Macro to place fields at specified offsets
+/// Creates a struct with fields placed at specific memory offsets.
+///
+/// This macro allows you to define a struct with precise memory layout by specifying
+/// the byte offset of each field. The macro automatically inserts padding between fields
+/// to ensure proper alignment.
+///
+/// # Optional Total Size
+///
+/// You can optionally specify the total size of the struct by adding a size value in
+/// square brackets after the struct name. This will ensure the struct has exactly that
+/// size by adding padding at the end if necessary.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust
+/// offset!(
+///     pub struct Example {
+///         0x0 pub field1: u32,
+///         0x4 pub field2: u16,
+///         0x8 pub field3: u64
+///     }
+/// );
+/// ```
+///
+/// With explicit total size:
+///
+/// ```rust
+/// offset!(
+///     pub struct ExampleWithSize[0x20] {
+///         0x0 pub field1: u32,
+///         0x4 pub field2: u16,
+///         0x8 pub field3: u64
+///         // The struct will be padded to exactly 0x20 bytes
+///     }
+/// );
+/// ```
+///
+/// For platform compatibility (e.g., Windows driver structures):
+///
+/// ```rust
+/// offset!(
+///     pub struct DEVICE_OBJECT[0x150] {
+///         0x0 pub type_: u16,
+///         0x2 pub size: u16,
+///         0x8 pub next: *mut DEVICE_OBJECT,
+///         // More fields...
+///     }
+/// );
+/// ```
 macro_rules! offset {
-    // Guarded munched exprs
-    // output
-    (@guard ($current_offset:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) =>    {
+    (@guard_with_size ($current_offset:expr, $struct_size:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) => {
+        $crate::paste::paste! {
+            #[repr(C, packed)]
+            $(#[$attr])* $vis struct $name {
+                $([<_pad $id>]: [u8;$amount], $vis_field $id: $ty,)*
+                _remaining_padding: [u8; $struct_size - $current_offset]
+            }
+        }
+    };
 
+    (@guard ($current_offset:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) => {
         $crate::paste::paste! {
             #[repr(C, packed)]
             $(#[$attr])* $vis struct $name { $([<_pad $id>]: [u8;$amount], $vis_field $id: $ty),* }
         }
-
     };
 
-    // add more fields
     (@guard ($current_offset:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty $(,)?) -> {$($output:tt)*}) => {
         offset!(@guard ($offset + core::mem::size_of::<$ty>()) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
     };
@@ -24,23 +79,107 @@ macro_rules! offset {
         offset!(@guard ($offset + core::mem::size_of::<$ty>(), $($next)+) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
     };
 
-    // Entry points
+    (@guard_with_size ($current_offset:expr, $struct_size:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty $(,)?) -> {$($output:tt)*}) => {
+        offset!(@guard_with_size ($offset + core::mem::size_of::<$ty>(), $struct_size) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
+    };
+
+    (@guard_with_size ($current_offset:expr, $struct_size:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty, $($next:tt)+) -> {$($output:tt)*}) => {
+        offset!(@guard_with_size ($offset + core::mem::size_of::<$ty>(), $struct_size, $($next)+) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
+    };
+
+
     ($(#[$attr:meta])* $vis:vis struct $struct_name:ident {$($input:tt)*}) => {
         offset!(@guard (0, $($input)*) -> {$(#[$attr])* $vis struct $struct_name});
         $crate::offset_checker!($struct_name {$($input)*});
     };
+
+    ($(#[$attr:meta])* $vis:vis struct $struct_name:ident[$struct_size:expr] {$($input:tt)*}) => {
+        offset!(@guard_with_size (0, $struct_size, $($input)*) -> {$(#[$attr])* $vis struct $struct_name});
+        $crate::offset_checker!($struct_name {$($input)*});
+    };
 }
 
-
 #[macro_export]
-/// Same as offset except using custom Debug implementation that behaves
-/// like the derived Debug implementation for this struct with the exception
-/// that the padding fields are not printed
+/// Creates a struct with fields at specific offsets and a custom Debug implementation.
+///
+/// This macro works the same as the `offset!` macro but also implements the Debug trait
+/// in a way that hides padding fields. This gives you cleaner debug output that shows
+/// only the actual data fields, not the padding.
+///
+/// # Optional Total Size
+///
+/// You can optionally specify the total size of the struct by adding a size value in
+/// square brackets after the struct name. This will ensure the struct has exactly that
+/// size by adding padding at the end.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```rust
+/// offset_debug!(
+///     pub struct Example {
+///         0x0 pub field1: u32,
+///         0x4 pub field2: u16,
+///         0x8 pub field3: u64
+///     }
+/// );
+///
+/// // Debug output will be something like:
+/// // Example { field1: 1, field2: 2, field3: 3 }
+/// // (no padding fields shown)
+/// ```
+///
+/// With explicit total size:
+///
+/// ```rust
+/// offset_debug!(
+///     pub struct KernelStructure[0x100] {
+///         0x00 pub header: u32,
+///         0x08 pub pointer: *mut u8,
+///         0x10 pub flags: u32
+///         // The struct will be padded to exactly 0x100 bytes
+///         // Debug output will not show the padding
+///     }
+/// );
+/// ```
+///
+/// Real-world example for Windows kernel structures:
+///
+/// ```rust
+/// offset_debug!(
+///     pub struct DRIVER_OBJECT[0x150] {
+///         0x0  pub type_: u16,
+///         0x2  pub size: u16,
+///         0x8  pub device_object: *mut DEVICE_OBJECT,
+///         0x10 pub flags: u32,
+///         0x18 pub driver_start: usize,
+///         // More fields...
+///     }
+/// );
+/// ```
 macro_rules! offset_debug {
-    // Guarded munched exprs
-    // output
-    (@guard ($current_offset:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) =>    {
 
+    (@guard_with_size ($current_offset:expr, $struct_size:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) => {
+        $crate::paste::paste! {
+            #[repr(C, packed)]
+            $(#[$attr])* $vis struct $name {
+                $([<_pad $id>]: [u8;$amount], $vis_field $id: $ty,)*
+                _remaining_padding: [u8; $struct_size - $current_offset]
+            }
+        }
+
+        impl core::fmt::Debug for $name {
+            fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                f.debug_struct(stringify!($name))
+                 $(.field(stringify!($id), unsafe { &core::ptr::read_unaligned(core::ptr::addr_of!(self.$id))}))*
+                 .finish()
+            }
+        }
+    };
+
+
+    (@guard ($current_offset:expr) -> {$(#[$attr:meta])* $vis:vis struct $name:ident $(($amount:expr, $vis_field:vis $id:ident: $ty:ty))*}) => {
         $crate::paste::paste! {
             #[repr(C, packed)]
             $(#[$attr])* $vis struct $name { $([<_pad $id>]: [u8;$amount], $vis_field $id: $ty),* }
@@ -55,7 +194,7 @@ macro_rules! offset_debug {
         }
     };
 
-    // add more fields
+
     (@guard ($current_offset:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty $(,)?) -> {$($output:tt)*}) => {
         offset_debug!(@guard ($offset + core::mem::size_of::<$ty>()) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
     };
@@ -64,9 +203,24 @@ macro_rules! offset_debug {
         offset_debug!(@guard ($offset + core::mem::size_of::<$ty>(), $($next)+) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
     };
 
-    // Entry points
+
+    (@guard_with_size ($current_offset:expr, $struct_size:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty $(,)?) -> {$($output:tt)*}) => {
+        offset_debug!(@guard_with_size ($offset + core::mem::size_of::<$ty>(), $struct_size) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
+    };
+
+    (@guard_with_size ($current_offset:expr, $struct_size:expr, $offset:literal $vis_field:vis $id:ident: $ty:ty, $($next:tt)+) -> {$($output:tt)*}) => {
+        offset_debug!(@guard_with_size ($offset + core::mem::size_of::<$ty>(), $struct_size, $($next)+) -> {$($output)* ($offset - ($current_offset), $vis_field $id: $ty)});
+    };
+
+
     ($(#[$attr:meta])* $vis:vis struct $struct_name:ident {$($input:tt)*}) => {
         offset_debug!(@guard (0, $($input)*) -> {$(#[$attr])* $vis struct $struct_name});
+        $crate::offset_checker!($struct_name {$($input)*});
+    };
+
+
+    ($(#[$attr:meta])* $vis:vis struct $struct_name:ident[$struct_size:expr] {$($input:tt)*}) => {
+        offset_debug!(@guard_with_size (0, $struct_size, $($input)*) -> {$(#[$attr])* $vis struct $struct_name});
         $crate::offset_checker!($struct_name {$($input)*});
     };
 }
@@ -74,16 +228,13 @@ macro_rules! offset_debug {
 #[cfg(feature = "checked")]
 #[macro_export]
 macro_rules! offset_checker {
-        ($struct_name:ident {$($offset:literal $vis_field:vis $id:ident: $ty:ty),* $(,)?}) => {
-            $(const _: ()  = assert!(core::mem::offset_of!($struct_name, $id) == $offset);)*
-        };
-
+    ($struct_name:ident {$($offset:literal $vis_field:vis $id:ident: $ty:ty),* $(,)?}) => {
+        $(const _: () = assert!(core::mem::offset_of!($struct_name, $id) == $offset);)*
+    };
 }
 
 #[cfg(not(feature = "checked"))]
 #[macro_export]
 macro_rules! offset_checker {
-        ($struct_name:ident {$($offset:literal $vis_field:vis $id:ident: $ty:ty),* $(,)?}) => {
-        };
-
+    ($struct_name:ident {$($offset:literal $vis_field:vis $id:ident: $ty:ty),* $(,)?}) => {};
 }
